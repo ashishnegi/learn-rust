53530eb4fe2e2ddbad32a887b99a3a89ca541e49
Ashish Negi <ashishnegi33@gmail.com>
2017-05-04 20:00:02 +0530

    Added Talkable trait

--- src/traits.rs
+++ src/traits.rs
@@ -8,6 +8,27 @@ fn largest<T: PartialOrd>(ts: &[T]) -> &T {
     large
 }
 
+trait Talkable {
+    fn say(&self) -> String {
+        String::from("i can talk.. a lot.")
+    }
+}
+
+struct Human {}
+struct Cat {}
+
+impl Talkable for Human {
+    fn say(&self) -> String {
+        String::from("i am human.. Are you also ?")
+    }
+}
+
+impl Talkable for Cat {
+    fn say(&self) -> String {
+        String::from("meow..")
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -15,5 +36,12 @@ mod tests {
     #[test]
     fn largest_test() {
         assert_eq!(4, *largest(&[1,2,3,4,2]));
+    }
+
+    #[test]
+    fn say_my_name() {
+        let human = Human{};
+        let cat = Cat{};
+        assert_ne!(human.say(), cat.say());
     }
 }
