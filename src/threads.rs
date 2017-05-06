cce1f1b77bc1a7651daf38fb1e35678d045a5eb5
Ashish Negi <ashishnegi33@gmail.com>
2017-05-07 01:12:35 +0530

    Channels CSP over STM

--- src/threads.rs
+++ src/threads.rs
@@ -1,4 +1,6 @@
 use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
 
 fn thread1() {
     let v = vec![1,2,3,4];
@@ -12,6 +14,44 @@ fn thread1() {
     }
 }
 
+fn channels() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            "hello",
+            "from",
+            "brave",
+            "new",
+            "world"
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::new(0,99999));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            "bye",
+            "from",
+            "weak",
+            "old",
+            "hell"
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::new(1,99999));
+        }
+    });
+
+    for v in rx {
+        println!("Got: {}", v);
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -20,5 +60,10 @@ mod tests {
     #[test]
     fn thread1_test() {
         thread1();
+    }
+
+    #[test]
+    fn channels_test() {
+        channels();
     }
 }
